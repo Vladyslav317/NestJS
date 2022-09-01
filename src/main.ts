51bc0c954bd298f3c46460faec8b96a52016de3c
Vladyslav <vladyslav3177@gmail.com>
2022-09-01 18:34:34 +0300

    changes

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 5000;
 
   const config = new DocumentBuilder()
     .setTitle('NestJS')
