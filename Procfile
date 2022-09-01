83783480a6f91c21883328f0904d332f514669f1
Vladyslav <vladyslav3177@gmail.com>
2022-09-01 18:22:15 +0300

    changes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm run start:prod
+web: npm run start:prod --port $PORT --host 0.0.0.0
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const PORT = process.env.PORT || 3000;
+
   const config = new DocumentBuilder()
     .setTitle('NestJS')
     .setDescription('NestJS API description')
@@ -32,6 +34,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(PORT);
 }
 bootstrap();
